f461f5814514c3104c5c2a516512518ba6208ca3
沈金木 <shenjinmu@shenjinmudeMacBook-Pro.local>
2021-05-14 01:59:39 +0800

    init

---
+++ pages/home/index.js
@@ -0,0 +1,92 @@
+// pages/home/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    countInfos: [
+      {
+        "name": "热门楼盘",
+        "value": 21,
+        "color": "#FF4452"
+      },
+      {
+        "name": "即将取证",
+        "value": 43,
+        "color": "#3087F9"
+      },
+      {
+        "name": "最新取证",
+        "value": 12,
+        "color": "#FB7432"
+      },
+      {
+        "name": "正在登记",
+        "value": 2,
+        "color": "#22C75C"
+      },
+      {
+        "name": "最新摇号",
+        "value": 15,
+        "color": "#B739FB"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
